e342b40f419b5f0e128ce0e12f00ff6a03d62b68
Artsiom <yrcraf@gmail.com>
2024-03-22 22:23:47 +0300

    tv slider

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+      <div className="tv-image-slider tv-moduleid-9969155"></div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
