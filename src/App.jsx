16b14bc3658e30ee05bde81373323a8534b10f6a
Artsiom <yrcraf@gmail.com>
2024-03-25 18:35:23 +0300

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Home } from './Views';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,29 +9,36 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      <div className="tv-image-slider tv-moduleid-9969155"></div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HM
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/the-mountains-does-not-walk' element={
+          <div>
+            <div>
+            {/* <div className="tv-image-slider tv-moduleid-9969155"></div> */}
+              <a href="https://vitejs.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+              </a>
+              <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+              </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </button>
+              <p>
+                Edit <code>src/App.jsx</code> and save to test HM
+              </p>
+            </div>
+            <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+            </p>
+          </div>}>
+        </Route>
+        <Route path = "/the-mountains-does-not-walk/sex" element={<Home/> }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Views/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Home.css'
+import palmL from '../../assets/palmL.png'
+import palmR from '../../assets/palmR.png'
+import compass from '../../assets/compass.png'
+import photos from '../../assets/photos.png'
+import rtL from '../../assets/rtL.png'
+import hta from '../../assets/hta.png'
+import rtR from '../../assets/rtR.png'
+import canon from '../../assets/canon.png'
+
+const Home = () => {
+    return (
+        <div className='homeLayout'>
+            <img className='appear' src={rtR} alt="" style={{position:'absolute', right:'0', top:'120px', animationDelay:'1.1s'}}/>
+            <img className='appear' src={rtL} alt="" style={{position:'absolute', left:'0', top:'45px', animationDelay:'0.5s'}}/>
+            <img className='slideL' src={palmL} alt="" style={{position:'absolute', left:'0', top:'0'}}/>
+            <img className='slideR' src={palmR} alt="" style={{position:'absolute', right:'0', top:'0'}}/>
+            <img className='appear scale' src={hta} alt="" style={{position:'absolute', right:'655px', top:'400px', animationDelay:'1.5s'}}/>
+            <img className='appear scale' src={canon} alt="" style={{position:'absolute', right:'520px', top:'-60px', animationDelay:'1.2s'}}/>
+            <img className='appear scale' src={compass} alt="" style={{position:'absolute', left:'370px', top:'290px', animationDelay:'0.8s'}}/>
+            <img className='appear scale' src={photos} alt="" style={{position:'absolute', left:'400px', top:'-55px', animationDelay:'1s'}}/>
+            <div className="tv-image-slider tv-moduleid-9969222"></div>
+
+        </div>
+    );
+};
+
+export default Home;
